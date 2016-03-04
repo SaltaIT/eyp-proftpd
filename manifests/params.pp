5c3303894f2bb983ff8b1985748de606ee5dc959
Jordi Prats <jprats@systemadmin.es>
2016-03-04 14:25:45 +0100

    suport centos

--- manifests/params.pp
+++ manifests/params.pp
@@ -20,6 +20,19 @@ class proftpd::params {
 					$proftpd_conf='/etc/proftpd.conf'
 
 					$modulepath_default='/usr/libexec/proftpd'
+
+					$load_tls=false
+					$load_site_misc=false
+					$load_tlsmemcache=false
+					$load_ctrlsadmin=false
+					$load_dynmasq=false
+					$load_uniqueid=false
+					$load_copy=false
+					$load_deflate=false
+					$load_ifversion=false
+
+					$user_default='ftp'
+					$group_default='ftp'
 				}
 				default: { fail("Unsupported RHEL/CentOS version! - ${::operatingsystemrelease}")  }
 			}
@@ -40,6 +53,20 @@ class proftpd::params {
 							$proftpd_conf='/etc/proftpd/proftpd.conf'
 
 							$modulepath_default='/usr/lib/proftpd'
+
+							$load_tls=true
+							$load_site_misc=true
+							$load_tlsmemcache=true
+							$load_ctrlsadmin=true
+							$load_dynmasq=true
+							$load_uniqueid=true
+							$load_copy=true
+							$load_deflate=true
+							$load_ifversion=true
+
+							$user_default='proftpd'
+							$group_default='nogroup'
+
             }
             default: { fail("Unsupported Ubuntu version! - ${::operatingsystemrelease}")  }
           }
