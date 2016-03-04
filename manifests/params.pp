5b8dae975217ce582c46635c0370cf7c8283fe51
Jordi Prats <jprats@systemadmin.es>
2016-03-04 12:41:09 +0100

    modules

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,8 +1,29 @@
 #
 class proftpd::params {
 
+	$systemlog_default='/var/log/proftpd/proftpd.log'
+
+	$confdir='/etc/proftpd/'
+	$modulesconf='/etc/proftpd/modules.conf'
+
   case $::osfamily
   {
+		'redhat':
+		{
+			case $::operatingsystemrelease
+			{
+				/^[5-7].*$/:
+				{
+					$proftpd_package='proftpd'
+					$include_epel=true
+
+					$proftpd_conf='/etc/proftpd.conf'
+
+					$modulepath_default='/usr/libexec/proftpd'
+				}
+				default: { fail("Unsupported RHEL/CentOS version! - ${::operatingsystemrelease}")  }
+			}
+		}
     'Debian':
     {
       case $::operatingsystem
@@ -14,6 +35,11 @@ class proftpd::params {
             /^14.*$/:
             {
               $proftpd_package='proftpd-basic'
+							$include_epel=false
+
+							$proftpd_conf='/etc/proftpd/proftpd.conf'
+
+							$modulepath_default='/usr/lib/proftpd'
             }
             default: { fail("Unsupported Ubuntu version! - ${::operatingsystemrelease}")  }
           }
