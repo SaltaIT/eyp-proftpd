9dd95240d9962a6342d2acc026bc7c3b28f22eac
Jordi Prats <jordi.prats@gmail.com>
2018-03-01 12:55:40 +0100

    proftpd user

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+## 0.2.11
+
+* added **ensure** for **proftpd::user**
+
 ## 0.2.10
 
 * added **PassivePorts** and **MasqueradeAddress**
--- manifests/user.pp
+++ manifests/user.pp
@@ -1,5 +1,6 @@
 # puppet2sitepp @ftpusers
 define proftpd::user(
+                      $ensure               = 'present',
                       $username             = $name,
                       $password             = undef,
                       $home                 = undef,
@@ -14,127 +15,148 @@ define proftpd::user(
                       $groups               = undef,
                       $login_ips            = undef,
                     ) {
-  #
-  if($chroot)
+  if($enseure=="present")
   {
-    if($restrict_ssh_to_sftp)
+    if($chroot)
     {
-      if ($groups == undef)
+      if($restrict_ssh_to_sftp)
       {
-        $user_groups=[ 'ftpchroot', 'sftp' ]
-      } else {
-        $user_groups=$groups
+        if ($groups == undef)
+        {
+          $user_groups=[ 'ftpchroot', 'sftp' ]
+        } else {
+          $user_groups=$groups
+        }
+        $require=Group[ 'ftpchroot', 'sftp' ]
       }
-      $require=Group[ 'ftpchroot', 'sftp' ]
+      else
+      {
+        if ($groups == undef)
+        {
+          $user_groups=[ 'ftpchroot' ]
+        } else {
+          $user_groups=$groups
+        }
+        $require=Group['ftpchroot']
+      }
     }
     else
     {
-      if ($groups == undef)
+      if($restrict_ssh_to_sftp)
       {
-        $user_groups=[ 'ftpchroot' ]
-      } else {
-        $user_groups=$groups
+        if ($groups == undef)
+        {
+          $user_groups=[ 'sftp' ]
+        } else {
+          $user_groups=$groups
+        }
+        $require=Group['sftp']
       }
-      $require=Group['ftpchroot']
+      else
+      {
+        if ($groups == undef)
+        {
+          $user_groups=undef
+        } else {
+          $user_groups=$groups
+        }
+        $require=undef
+      }
     }
-  }
-  else
-  {
+
+    if($login_ips!=undef)
+    {
+      validate_array($login_ips)
+
+      proftpd::class { "loginips_${username}":
+        ip => $login_ips,
+      }
+
+      ->
+
+      proftpd::limitlogin { "limit_login_${username}":
+        limituser  => [ $username ],
+        allowclass => "loginips_${username}",
+      }
+
+    }
+
     if($restrict_ssh_to_sftp)
     {
-      if ($groups == undef)
+      if(!defined(Group['sftp']))
       {
-        $user_groups=[ 'sftp' ]
-      } else {
-        $user_groups=$groups
-      }
-      $require=Group['sftp']
-    }
-    else
-    {
-      if ($groups == undef)
-      {
-        $user_groups=undef
-      } else {
-        $user_groups=$groups
-      }
-      $require=undef
-    }
-  }
-
-  if($login_ips!=undef)
-  {
-    validate_array($login_ips)
-
-    proftpd::class { "loginips_${username}":
-      ip => $login_ips,
-    }
-
-    ->
-
-    proftpd::limitlogin { "limit_login_${username}":
-      limituser  => [ $username ],
-      allowclass => "loginips_${username}",
-    }
-
-  }
-
-  if($restrict_ssh_to_sftp)
-  {
-    if(!defined(Group['sftp']))
-    {
-      group { 'sftp':
-        ensure => 'present',
+        group { 'sftp':
+          ensure => 'present',
+        }
       }
     }
-  }
 
-  if ($groups != undef)
-  {
-    validate_array($groups)
-  }
-
-  user { $username:
-    uid        => $uid,
-    gid        => $gid,
-    password   => $password,
-    managehome => $managehome,
-    home       => $home,
-    allowdupe  => $allowdupe,
-    groups     => $user_groups,
-    shell      => $shell,
-    require    => $require,
-    membership => inclusive,
-  }
-
-  if($disable_ssh_user)
-  {
-    if(defined(Class['openssh::server']))
+    if ($groups != undef)
     {
-      openssh::denyuser { $username: }
+      validate_array($groups)
     }
-    else
-    {
-      fail('unable to disable ssh, class openssh::server not defined')
+
+    user { $username:
+      uid        => $uid,
+      gid        => $gid,
+      password   => $password,
+      managehome => $managehome,
+      home       => $home,
+      allowdupe  => $allowdupe,
+      groups     => $user_groups,
+      shell      => $shell,
+      require    => $require,
+      membership => inclusive,
     }
-  }
-  else
-  {
-    if($restrict_ssh_to_sftp)
+
+    if($disable_ssh_user)
     {
       if(defined(Class['openssh::server']))
       {
-        openssh::match { "match openssh ${username} ${uid}":
-          users           => [$username],
-          forcecommand    => $::openssh::params::sftp_server,
-          chrootdirectory => '%h',
-          require         => Group['sftp'],
-        }
+        openssh::denyuser { $username: }
       }
-      else {
-        fail('unable to enable sftp, class openssh::server not defined')
+      else
+      {
+        fail('unable to disable ssh, class openssh::server not defined')
+      }
+    }
+    else
+    {
+      if($restrict_ssh_to_sftp)
+      {
+        if(defined(Class['openssh::server']))
+        {
+          openssh::match { "match openssh ${username} ${uid}":
+            users           => [$username],
+            forcecommand    => $::openssh::params::sftp_server,
+            chrootdirectory => '%h',
+            require         => Group['sftp'],
+          }
+        }
+        else {
+          fail('unable to enable sftp, class openssh::server not defined')
+        }
       }
     }
   }
-
+  elsif ($ensure=='absent')
+  {
+    user { $username:
+      enseure    => $ensure,
+      uid        => $uid,
+      gid        => $gid,
+      password   => $password,
+      managehome => $managehome,
+      home       => $home,
+      allowdupe  => $allowdupe,
+      groups     => $user_groups,
+      shell      => $shell,
+      require    => $require,
+      membership => inclusive,
+    }
+  }
+  else
+  {
+    fail("unsupported ensure: ${ensure} valid: present/absent")
+  }
 }
